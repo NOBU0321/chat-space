c46fe6c687460d70efeab4e8b859bfc6906f531a
NOBU0321 <60180339+NOBU0321@users.noreply.github.com>
2020-04-03 12:12:56 +0900

    Viewの修正
    View崩れの修正を行う

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,40 +2,40 @@ $(function(){
     function buildHTML(message){
     if ( message.image ) {
       var html =
-        `<div class="messages">
-          <div class="message-box">
-            <div class="message-box__upper-info">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
-              ${message.created_at}
-            </div>
-          </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-          </div>
-          <img src=${message.image} >
-        </div>`
+      `<div class="message-box">
+        <div class="message-box__upper-info">
+          <p class="message-box__upper-info__user-name">
+            ${message.user_name}
+          </p>
+          <p class="message-box__upper-info__date">
+            ${message.created_at}
+          </p>
+        </div>
+        <div class="lower-message">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+        </div>
+        <img src=${message.image} >
+      </div>`
       return html;
     } else {
       var html =
-        `<div class="message">
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
-              ${message.created_at}
-            </div>
-          </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-          </div>
-        </div>`
+      `<div class="message-box">
+        <div class="message-box__upper-info">
+          <p class="message-box__upper-info__user-name">
+            ${message.user_name}
+          </p>
+          <p class="message-box__upper-info__date">
+            ${message.created_at}
+          </p>
+        </div>
+        <div class="lower-message">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
       return html;
     };
   }
